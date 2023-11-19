7ae389ad33c9b7a1447aae2c35490543c72acb2c
Mahesh Sirsat <70777516+MaheshSirsat@users.noreply.github.com>
2023-11-19 22:01:50 +0530

    Add files via upload

---
+++ Assignment-2/view/myjsview.view.js
@@ -0,0 +1,18 @@
+sap.ui.jsview('com.sapui5.view.myjsview',{
+    getControllerName(){
+        return "com.sapui5.Controller.mycontroller"
+    },
+    createContent(oController){
+        var Input=new sap.m.Input('idjsinput',{value:''})
+        var Obtn=new sap.m.Button('myjsbtn',{
+            text:'JSButton',
+            // press:oController.jsbutton
+        })
+        var Obtn2=new sap.m.Button('idjsinput2',{
+            text:'EnableJSButton',
+            press:[oController.jsbutton,oController]
+        })
+        return [Input,Obtn,Obtn2]
+
+    }
+})
